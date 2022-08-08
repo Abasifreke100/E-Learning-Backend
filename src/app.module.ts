82c72dcf051fb20930ed7a06814097880ee6668b
mitchel <edahmitchel@example.com>
2022-08-08 16:41:25 +0100

    created_anything_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersService } from './orders/orders.service';
+import { AnythingController } from './anything/anything.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AnythingController],
   providers: [AppService, OrdersService],
 })
 export class AppModule {}
