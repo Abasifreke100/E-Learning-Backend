6ee727d48480b285150727b6eea194c1eb135fe0
mitchel <edahmitchel@example.com>
2022-08-15 12:12:53 +0100

    cleaned dev branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrdersService } from './orders/orders.service';
-import { AnythingController } from './anything/anything.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController, AnythingController],
-  providers: [AppService, OrdersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
