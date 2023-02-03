2a1de54a4f652b01d550282a8818bd421e6e2188
JC-Coder <josephchimezie2003@gmail.com>
2023-02-03 13:19:32 +0100

    refactored project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
-import { typeOrmConfig } from './config/typeorm.config';
+import { typeOrmConfig } from './common/config/typeorm.config';
 import { User } from './models/user.entity';
 import { AuthModule } from './modules/v1/auth/auth.module';
 import { UserModule } from './modules/v1/user/user.module';
